4bf3cb1ea69a34230ae72ddb8ca4fc87defcd153
mokuzaru <kenny.otoya@pucp.pe>
2019-09-07 19:22:40 -0500

    delete links

--- src/routes/links.js
+++ src/routes/links.js
@@ -15,6 +15,17 @@ router.post('/add', async(req,res) => {
     description
   };
   await pool.query('INSERT INTO links set ?', [newLink]);
-  res.send('recived');
+  res.redirect('/links');
+});
+
+router.get('/', async (req, res) =>{
+  const links = await pool.query('SELECT * FROM links');
+  res.render('links/list', {links});
+});
+
+router.get('/delete/:id', async(req, res) =>{
+  const {id} = req.params;
+  await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+  res.redirect('/links');
 });
 module.exports = router;
