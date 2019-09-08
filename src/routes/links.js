d38a557269ad5be8f77472e7bd7249c17dac9ce4
mokuzaru <kenny.otoya@pucp.pe>
2019-09-07 20:55:28 -0500

    edit config

--- src/routes/links.js
+++ src/routes/links.js
@@ -28,4 +28,22 @@ router.get('/delete/:id', async(req, res) =>{
   await pool.query('DELETE FROM links WHERE ID = ?', [id]);
   res.redirect('/links');
 });
+
+router.get('/edit/:id', async(req, res) =>{
+  const { id } = req.params;
+  const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+  res.render('links/edit', {link:links[0]});
+});
+
+router.post('/edit/:id', async(req, res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newLink = {
+    title,
+    description,
+    url
+  };
+  await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+  res.redirect('/links')
+});
 module.exports = router;
