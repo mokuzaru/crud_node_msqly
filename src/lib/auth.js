78c3785af0841dc034ff4015134d286fa4109ba3
mokuzaru <kenny.otoya@pucp.pe>
2019-09-08 02:49:06 -0500

    verifi routes

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,5 +5,12 @@ module.exports = {
       return next();
     }
     return res.redirect('/signin');
+  },
+
+  isNotLoggedIn (req, res, next) {
+    if(!req.isAuthenticated()){
+      return next();
+    }
+    return res.redirect('/profile');
   }
 };
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { isLoggedIn} = require('../lib/auth');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
-router.get('/signup', (req, res) =>{
+router.get('/signup', isNotLoggedIn, (req, res) =>{
   res.render('auth/signup');
 });
 
@@ -19,17 +19,17 @@ router.get('/signup', (req, res) =>{
 //});
 
 //Other way
- router.post ('/signup', passport.authenticate('local.signup', {
+ router.post ('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
    successRedirect: '/profile',
    failureRedirect: '/signup',
    failureFlash: true
  }));
 
- router.get('/signin', (req, res) => {
+ router.get('/signin', isNotLoggedIn, (req, res) => {
    res.render('auth/signin');
  });
 
- router.post('/signin', (req, res, next) => {
+ router.post('/signin', isNotLoggedIn, (req, res, next) => {
    passport.authenticate('local.signin', {
      successRedirect: '/profile',
      failureRedirect: '/signin',
@@ -41,7 +41,7 @@ router.get('/profile', isLoggedIn, (req,res) => {
   res.render('profile');
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', isLoggedIn, (req, res) => {
   req.logOut();
   res.redirect('/signin');
 });
